a94f29796131963d5a576154b9addda87ba4a76d
YoonHaneul2010 <66522887+YoonHaneul2010@users.noreply.github.com>
2020-09-09 01:32:38 +0900

    Add files via upload
    ATM 번역 수정본

--- vrp_atm_rob-master/client.lua
+++ vrp_atm_rob-master/client.lua
@@ -9,7 +9,7 @@ end
 
 function toggleMenu(b,send)
     if timeout>0 then
-        vRP.notify({"기다리세요!"})
+        vRP.notify({"기다리십시오!"})
     else
         menu = b
         SetNuiFocus(b,b)
@@ -19,7 +19,7 @@ function toggleMenu(b,send)
 end
 
 RegisterNUICallback("lose",function(data,cb)
-    vRP.notify({"~r~은행 문서가 잉크로 인해 손상되었습니다!"})
+    vRP.notify({"~r~당신은 ATM 해킹에 실패 하였습니다..."})
     toggleMenu(false,false)
     timeout = Config.timeout
 end)
@@ -31,7 +31,7 @@ end)
 
 RegisterNUICallback("win",function(data,cb)
     	TriggerServerEvent("fn_atm_rob:win",function()
-        vRP.notitfy({"~g~당신은 ATM 해킹에 성공하였습니다!"})
+        vRP.notitfy({ "~g~당신은 ATM 해킹에 성공 하였습니다!"})
         toggleMenu(false,false)
         timeout = Config.timeout
     end)
@@ -40,7 +40,7 @@ end)
 function disp_time(time)
     local minutes = math.floor((time%3600)/60)
     local seconds = math.floor(time%60)
-    return string.format("~b~%d ~s~분(초) ~b~%02d~s~ 초(초)",분,초)
+    return string.format("~b~%d ~s~minute(s) ~b~%02d~s~ second(s)",minute,second)
 end
 
 Citizen.CreateThread(function()
@@ -61,7 +61,7 @@ Citizen.CreateThread(function()
                 DrawMarker(1, v.x, v.y, v.z-1.0, 0, 0, 0, 0, 0, 0, marker.size.x, marker.size.y, marker.size.z, marker.color.r, marker.color.g, marker.color.b, marker.color.alpha, 0, 0, 0, false)
                 if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), vector3(v.x,v.y,v.z), false)<marker.size.x then
                     if not menu then 
-                        atm_DisplayHelpText("해킹 장치를 ~INPUT_CONTEXT~ 키를 눌러 넣으십시오.") 
+                        atm_DisplayHelpText(" ~INPUT_CONTEXT~ 키를 눌러 해킹장치를 삽입하세요!") 
                     end
                     if IsControlJustPressed(0, 51) and not menu then
                         toggleMenu(true,true)
